cc14c4199150122db2a169dccf067dedebdc1630
narraboss <basanth.narra@gmail.com>
2019-09-18 22:12:42 -0400

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,17 +2,20 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    image: 'https://isha.sadhguru.org/blog/wp-content/uploads/2016/05/natures-temples.jpg'
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    image: 'https://isha.sadhguru.org/blog/wp-content/uploads/2016/05/natures-temples.jpg'
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    image: 'https://isha.sadhguru.org/blog/wp-content/uploads/2016/05/natures-temples.jpg'
   }
 ];
 
